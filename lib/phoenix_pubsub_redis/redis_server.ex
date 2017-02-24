4d1951d7a6deb1b5f9b3bca61f65cf1c09e9cdf8
robmadole <robmadole@gmail.com>
2017-02-24 13:23:53 -0600

    Temporarily loosen the disconnect handler

--- lib/phoenix_pubsub_redis/redis_server.ex
+++ lib/phoenix_pubsub_redis/redis_server.ex
@@ -69,8 +69,8 @@ defmodule Phoenix.PubSub.RedisServer do
     {:noreply, state}
   end
 
-  def handle_info({:redix_pubsub, redix_pid, :disconnected, %{reason: reason}}, %{redix_pid: redix_pid} = state) do
-    Logger.error "Phoenix.PubSub disconnected from Redis with reason #{inspect reason} (awaiting reconnection)"
+  def handle_info({:redix_pubsub, redix_pid, :disconnected, %{reason: reason}}, state) do
+    Logger.error "PubSub disconnected because of #{inspect(state, pretty: true)}"
     {:noreply, state}
   end
 
